5a67e9d92149996f0c3157363d6be25bf78a28e8
maidishike <bixiao1023.tum@gmail.com>
2018-08-26 02:25:10 +0200

    update

--- example/example.js
+++ example/example.js
@@ -1,79 +1,70 @@
-const secUtils = require('@sec-block/secjs-util')
-const utils = new secUtils()
-const isBrowser = typeof process === "undefined" || !process.nextTick || Boolean(process.browser)
-const sjcl = require("sjcl")
-const uuid = require("uuid")
-const secp256k1 = require("secp256k1/elliptic")
-const createKeccakHash = require("keccak/js")
-const crypto = isBrowser ? require("crypto-browserify") : require("crypto")
+const SecUtils = require('@sec-block/secjs-util')
+const utils = new SecUtils()
+const isBrowser = typeof process === 'undefined' || !process.nextTick || Boolean(process.browser)
+const sjcl = require('sjcl')
+const uuid = require('uuid')
+const secp256k1 = require('secp256k1/elliptic')
+const createKeccakHash = require('keccak/js')
+const crypto = isBrowser ? require('crypto-browserify') : require('crypto')
 
-const secKeys = require("../src/index.js")
-const keySEC = new secKeys()
+const SecKeys = require('../src/index.js')
+const keySEC = new SecKeys()
 
 const constants = {
-    // Symmetric cipher for private key encryption
-    cipher: "aes-128-ctr",
+  // Symmetric cipher for private key encryption
+  cipher: 'aes-128-ctr',
 
-    // Initialization vector size in bytes
-    ivBytes: 16,
+  // Initialization vector size in bytes
+  ivBytes: 16,
 
-    // ECDSA private key size in bytes
-    keyBytes: 32,
+  // ECDSA private key size in bytes
+  keyBytes: 32,
 
-    // Key derivation function parameters
-    pbkdf2: {
-        c: 262144,
-        dklen: 32,
-        hash: "sha256",
-        prf: "hmac-sha256"
-    },
-    scrypt: {
-        memory: 280000000,
-        dklen: 32,
-        n: 262144,
-        r: 1,
-        p: 8
-    }
+  // Key derivation function parameters
+  pbkdf2: {
+    c: 262144,
+    dklen: 32,
+    hash: 'sha256',
+    prf: 'hmac-sha256'
+  },
+  scrypt: {
+    memory: 280000000,
+    dklen: 32,
+    n: 262144,
+    r: 1,
+    p: 8
+  }
 }
 
-
-//  get SEC privateKey 32bit hex 
+//  get SEC privateKey 32bit hex
 const privateKey = utils.getPrivateKey()
-a = console.log('privateKey:', privateKey)
-//  get eth privateKey 32hex 
+let a = console.log('privateKey:', privateKey)
+//  get eth privateKey 32 hex
 let ethPrivateKey = privateKey.slice(0, 32)
-b = console.log('ethPrivateKey:', ethPrivateKey)
+let b = console.log('ethPrivateKey:', ethPrivateKey)
 
 let x = keySEC.create(constants, (err, value) => {
-    if (err) {
-        console.log('private key, initialization vector and salt (for key derivation)',err) // 'error occurs:',
-    } else {
-        console.log(value)
-    }
+  if (err) {
+    console.log('private key, initialization vector and salt (for key derivation)', err) // 'error occurs:',
+  } else {
+    console.log(value)
+  }
 })
 
-// let iv x.iv 
+// let iv x. iv
 console.log(keySEC.isHex(privateKey))
 
 //  create initialization vector and salt (for key derivation).
 // c = keySEC.create(cb)
 // console.log(c)
 
-
-
-
-
-
-//const derivedKey = keySEC.deriveKey(bi7012xiao, )
+// const derivedKey = keySEC.deriveKey(bi7012xiao, )
 // b= console.log('deriveKey:',derivedKey)
 
-
-// const version = "1.0.4",
+// const version = '1.0.4',
 
 //  const browser = isBrowser,
 
 // const scrypt = null,
-
-
 
 // a= console.log('crypto'+ crypto)
